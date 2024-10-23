22c84c6519651c45ebb7927af8fe43973d11053e
Yevhenii Pozniak <jekamanu@gmail.com>
2024-10-23 21:13:42 +0300

    add header and navigation
    routing for navigation

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <section>
+      <h1>This is home</h1>
+    </section>
+  );
+}
---
+++ src/pages/MyStory/MyStory.jsx
@@ -0,0 +1,7 @@
+export default function MyStory() {
+  return (
+    <div>
+      <h1>This is my story page</h1>
+    </div>
+  );
+}
